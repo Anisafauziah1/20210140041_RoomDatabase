82be1838d812db538b7b5b03eaf2c7191783f47e
Anisafauziah1 <fzhnsa@gmail.com>
2023-12-06 15:46:37 +0700

    mmenambahkan strings

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
@@ -37,4 +38,11 @@ data class DetailSiswa(
     val nama: String ="",
     val alamat: String ="",
     val telpon: String ="",
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
