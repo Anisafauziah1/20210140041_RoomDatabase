3de0286da9278bebd57b8236bb7116cef3ae2548
Anisafauziah1 <fzhnsa@gmail.com>
2023-12-06 16:11:09 +0700

    create fun CreationExtras

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.roomsiswa.model
 
 import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 
@@ -15,3 +17,4 @@ object PenyediaViewModel{
     }
 }
 
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
